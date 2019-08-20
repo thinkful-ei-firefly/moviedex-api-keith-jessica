57e0f4a9e4aa571054762190b4c55d0d8a25f7df
Jess Doyle <jessica.e.doyle@gmail.com>
2019-08-20 14:32:16 -0400

    3rd party packages added

--- app.js
+++ app.js
@@ -1,16 +1,31 @@
 'use strict';
 
+require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
+const helmet = require('helmet');
 
 const movieData = require('./movieData');
 
+//console.log(process.env.API_KEY); => 'secret-key'
+
 const app = express();
 
+function validateBearerToken(req, res, next) {
+  console.log('checking bearer token...');
+  next();
+}
+
+
+app.use(cors());
+app.use(helmet());
 app.use(morgan('dev'));
+app.use(validateBearerToken);
+
 
 app.get('/movie', (req, res) => {
-  
+
   return res.json(movieData);
 });
 
